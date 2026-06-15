8a7bcddfb707ea42b2212171585f6f2c30fdc3ef
HARDIK ARORA <arorahardik0811@gmail.com>
2026-06-15 22:10:14 +0530

    Update go-prerequsites.sh

--- go-prerequsites.sh
+++ go-prerequsites.sh
@@ -1,10 +1,16 @@
-cd ~/Otel-Jaegar
+
+rm -f go.mod go.sum
+
 go mod init my-go-app
 
-go get go.opentelemetry.io/otel
-go get go.opentelemetry.io/otel/sdk
-go get go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc
+
+go get go.opentelemetry.io/otel@v1.24.0
+go get go.opentelemetry.io/otel/sdk@v1.24.0
+go get go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc@v1.24.0
+
 
 go mod tidy
 
+
 docker build -t my-go-app:v1 .
+docker run my-go-app
