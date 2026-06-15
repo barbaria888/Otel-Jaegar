cb19d4e00407abb0a848d7ecb943b1038e46aa90
HARDIK ARORA <arorahardik0811@gmail.com>
2026-06-15 22:47:40 +0530

    Update go-prerequsites.sh

--- go-prerequsites.sh
+++ go-prerequsites.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 
 rm -f go.mod go.sum
 
@@ -11,6 +13,13 @@ go get go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc@v1.24.0
 
 go mod tidy
 
-kubectl apply -f go-app-deployment.yaml
+
 kubectl apply -f jaegar-deployment.yaml
+
+
+kubectl apply -f otel-collector.yaml
+
+echo "☸️ Step 6: Deploying Live Looping Go Trace Generator Application..."
+kubectl apply -f go-app-deployment.yaml
+
 
